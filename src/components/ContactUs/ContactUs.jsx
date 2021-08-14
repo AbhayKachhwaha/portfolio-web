ba5cb298c5ba49f6201bd352da502fc6014f6a9a
AbhayKachhwaha <abhay.kachhwah09@gmail.com>
2021-08-14 19:16:32 +0530

    Added Contact Us form

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import "./ContactUs.css";
+import { ButtonLink as Button } from "../ButtonElement";
 
 const ContactUs = () => {
-    return (
-        <div>
-            <h1>Contact Me</h1>
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="contact-us">
+      <h1>Contact Me</h1>
+      <form onSubmit={submitHandler}>
+        <div className="form-control">
+          <label>
+            Name
+            <input type="text" name="name" maxlength="30" />
+          </label>
+          <label>
+            Company Name
+            <input type="text" name="companyname" maxlength="30" />
+          </label>
+          <label>
+            E-mail
+            <input type="email" name="email" />
+          </label>
+          <label>
+            Project Description
+            <input type="text" name="project" maxlength="500" />
+          </label>
+          <div className="button-wrap">
+            <Button type="submit" to="#" lightBg={false} primary={false}>
+              Submit
+            </Button>
+          </div>
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
 
 export default ContactUs;
