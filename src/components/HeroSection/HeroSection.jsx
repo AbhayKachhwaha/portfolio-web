51f07ca6f0dda4ac1e667db64fb2c24626d491b7
AbhayKachhwaha <abhay.kachhwah09@gmail.com>
2021-08-29 01:07:22 +0530

    Changed to compressed video

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,32 +1,41 @@
 // import React, {useState} from 'react';
-import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP } from './HeroElements';
-import Video from '../../videos/video.mp4';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+} from "./HeroElements";
+import Video from "../../videos/video.m4v";
 // import {Button} from '../../components/ButtonElement';
 
 const HeroSection = () => {
+  // const [hover, setHover] = useState(false);
 
-    // const [hover, setHover] = useState(false);
+  //  const onHover = () => {
+  //     setHover(!hover);
+  //  };
 
-    //  const onHover = () => {
-    //     setHover(!hover);
-    //  };
-     
-    return (
-        <HeroContainer>
-            <HeroBg>
-                <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
-            </HeroBg>
-            <HeroContent>
-                <HeroH1>Choose me for your business needs</HeroH1>
-                <HeroP>I will provide solutions for your business ideas in an efficient manner</HeroP>
-                {/* <HeroBtnWrapper>
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/m4v" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Choose me for your business needs</HeroH1>
+        <HeroP>
+          I will provide solutions for your business ideas in an efficient
+          manner
+        </HeroP>
+        {/* <HeroBtnWrapper>
                     <Button to='contactus' onMouseEnter={onHover} onMouseLeave={onHover}>
                         Contact Us {hover ? <ArrowForward /> : <ArrowRight />}
                     </Button>
                 </HeroBtnWrapper> */}
-            </HeroContent>
-        </HeroContainer>
-    )
-}
+      </HeroContent>
+    </HeroContainer>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
