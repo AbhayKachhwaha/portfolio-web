f25455e9da0c9b2f9927d9be7d4be1f06b06e74c
AbhayKachhwaha <abhay.kachhwah09@gmail.com>
2021-09-07 14:07:16 +0530

    Fixed card image and new project added

--- src/components/Cards/ProjectData.js
+++ src/components/Cards/ProjectData.js
@@ -1,5 +1,6 @@
 import weatherprojectimg from "../../images/Project1.jpg";
-import chatprojectimg from '../../images/chatproject.PNG';
+import chatprojectimg from "../../images/chatproject.PNG";
+import fooddelprojimg from "../../images/foodDeliverApp.PNG";
 
 export const projectData = [
   {
@@ -7,55 +8,55 @@ export const projectData = [
     imgsrc: weatherprojectimg,
     alttext: "Weather PWA",
     projectTitle: "Weather PWA",
-    projectDescription: "A weather report progressive web application"
-  }, 
-  {
-    link: "https://chat-app-akachhwaha.netlify.app/",
-    imgsrc: chatprojectimg,
-    alttext: "Chat Application",
-    projectTitle: "Chat Application",
-    projectDescription: "A chatting website with multimedia"
+    projectDescription: "A weather report progressive web application",
   },
   {
     link: "https://chat-app-akachhwaha.netlify.app/",
     imgsrc: chatprojectimg,
     alttext: "Chat Application",
     projectTitle: "Chat Application",
-    projectDescription: "A chatting website with multimedia"
+    projectDescription: "A chatting website with multimedia and read reciepts",
+  },
+  {
+    link: "https://food-order-react-app.netlify.app/",
+    imgsrc: fooddelprojimg,
+    alttext: "Food Delivery Application",
+    projectTitle: "Food Delivery Application",
+    projectDescription: "A Food Delivery Application",
   },
   {
     link: "https://chat-app-akachhwaha.netlify.app/",
     imgsrc: chatprojectimg,
     alttext: "Chat Application",
     projectTitle: "Chat Application",
-    projectDescription: "A chatting website with multimedia"
+    projectDescription: "A chatting website with multimedia",
   },
   {
     link: "https://chat-app-akachhwaha.netlify.app/",
     imgsrc: chatprojectimg,
     alttext: "Chat Application",
     projectTitle: "Chat Application",
-    projectDescription: "A chatting website with multimedia"
+    projectDescription: "A chatting website with multimedia",
   },
   {
     link: "https://chat-app-akachhwaha.netlify.app/",
     imgsrc: chatprojectimg,
     alttext: "Chat Application",
     projectTitle: "Chat Application",
-    projectDescription: "A chatting website with multimedia"
+    projectDescription: "A chatting website with multimedia",
   },
   {
     link: "https://chat-app-akachhwaha.netlify.app/",
     imgsrc: chatprojectimg,
     alttext: "Chat Application",
     projectTitle: "Chat Application",
-    projectDescription: "A chatting website with multimedia"
+    projectDescription: "A chatting website with multimedia",
   },
   {
     link: "https://chat-app-akachhwaha.netlify.app/",
     imgsrc: chatprojectimg,
     alttext: "Chat Application",
     projectTitle: "Chat Application",
-    projectDescription: "A chatting website with multimedia"
+    projectDescription: "A chatting website with multimedia",
   },
 ];
