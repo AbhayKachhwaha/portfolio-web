a1cb2793e181883a4dd4150ebaa7fbb1c83399fa
AbhayKachhwaha <abhay.kachhwah09@gmail.com>
2022-10-26 13:16:00 -0400

    Updated project data

--- src/components/Cards/ProjectData.js
+++ src/components/Cards/ProjectData.js
@@ -9,14 +9,23 @@ export const projectData = [
     imgsrc: blogwebimg,
     alttext: "Blog Website/Template",
     projectTitle: "Blog Website/Template",
-    projectDescription: "A super-fast blogging website built using Modern Technologies with CMS",
+    projectDescription:
+      "A super-fast blogging website built using Modern Technologies with CMS",
   },
   {
     link: "https://weatherpwareact99.netlify.app/",
     imgsrc: weatherprojectimg,
     alttext: "Weather PWA",
     projectTitle: "Weather PWA",
-    projectDescription: "A weather report progressive web application",
+    projectDescription: "A weather report progressive web application that can be downloaded on any device from the browser",
+  },
+  {
+    link: "https://food-order-react-app.netlify.app/",
+    imgsrc: fooddelprojimg,
+    alttext: "Food Delivery Application example",
+    projectTitle: "Food Delivery Application Example",
+    projectDescription:
+      "A Food Delivery Application template front-end designed with REACTJS",
   },
   {
     link: "https://chat-app-akachhwaha.netlify.app/",
@@ -24,12 +33,5 @@ export const projectData = [
     alttext: "Chat Application",
     projectTitle: "Chat Application",
     projectDescription: "A chatting website with multimedia and read reciepts",
-  },
-  {
-    link: "https://food-order-react-app.netlify.app/",
-    imgsrc: fooddelprojimg,
-    alttext: "Food Delivery Application",
-    projectTitle: "Food Delivery Application",
-    projectDescription: "A Food Delivery Application designed with REACT",
   },
 ];
